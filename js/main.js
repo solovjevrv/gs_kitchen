1f3847233bd2285c27dc6e370a76afd6ddbe6a16
Roman <solovjevrv@gmail.com>
2019-04-12 21:10:44 +0300

    начало работы над content-block + swiper

--- js/main.js
+++ js/main.js
@@ -4,3 +4,33 @@ $(document).ready(function(){
     $(".desktop_nav").fadeToggle(500);
   });
 });
+
+// инициализация swiper
+var mySwiper = new Swiper ('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerGroup: 1,
+  slidesPerColumnFill: 'row',
+  centeredSlides: true,
+  slidesPerView: 3,
+  autoplay: {delay: 5000},
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
